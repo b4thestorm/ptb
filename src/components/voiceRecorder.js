c6346229b9aa340c47ce307ea57acb8bea6afb3b
arnoldsanders <arnoldsander@gmail.com>
2025-03-30 22:25:57 -0400

    play audio, delete audio

--- src/components/voiceRecorder.js
+++ src/components/voiceRecorder.js
@@ -1,12 +1,16 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import { Box, Typography, Stack } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import MicIcon from '@mui/icons-material/Mic';
 import PauseIcon from '@mui/icons-material/Pause';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import DeleteIcon from '@mui/icons-material/Delete';
 import  useAudioRecorder  from '../hooks/useAudioRecorder';
 
 export default function VoiceRecorder () {
     const [audioUrl, setAudioUrl] = useState()
+    const [available, setAvailable] = useState(false)
+    const playbackRef = useRef()
     const {
         startRecording,
         stopRecording,
@@ -16,19 +20,18 @@ export default function VoiceRecorder () {
     } = useAudioRecorder();
 
 
-    //  ADD THE CAPABILITY TO PLAYBACK THE RECORDING
     useEffect(()=> {
         if (recordingBlob) {
-            console.log(recordingBlob)
             let url = URL.createObjectURL(recordingBlob);
             setAudioUrl(url)
+            setAvailable(true)
         } 
     }, [recordingBlob]) 
 
 
   return (
     <Box>
-        <audio controls src={recordingBlob ? audioUrl : null}></audio>
+        <audio controls src={recordingBlob ? audioUrl : null} ref={playbackRef} style={{'visibility': 'hidden'}}></audio>
         <Stack direction={"row"} gap={2}>
         <IconButton
             onClick={togglePauseResume}
@@ -52,6 +55,37 @@ export default function VoiceRecorder () {
                 aria-label={isRecording ? "Stop recording" : "Start recording"}
             >
                 <MicIcon fontSize="large" color={"white"} />
+        </IconButton>
+        <IconButton
+            onClick={()=> {
+                playbackRef.current.play()
+            }}
+            sx={{
+                border: '2px solid',
+                borderColor: 'primary.main',
+                '&:hover': {
+                    backgroundColor: 'action.hover'
+                },
+                animation: isRecording ? 'pulse 1.5s infinite' : 'none'
+                }}
+
+                aria-label={isRecording ? "Stop recording" : "Start recording"}
+            >
+                <PlayArrowIcon fontSize="large" color={"white"} />
+        </IconButton>
+        <IconButton
+            onClick={()=> {
+                setAudioUrl(null)
+            }}
+            sx={{
+                border: '2px solid',
+                borderColor: 'primary.main',
+                '&:hover': {
+                    backgroundColor: 'action.hover'
+                }
+                }}
+            >
+                <DeleteIcon fontSize="large" color={"white"} />
         </IconButton>
         </Stack>
     </Box>
